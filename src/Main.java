485642546884571dc4af90b6e1bc89f06e3e4353
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:46:43 +0530

    Use-If-Statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int temp = 40;
+        if(temp > 40){
+            System.out.println("Hot day");
+        }else if(temp > 20){
+            System.out.println("Beautiful day");
+        }else {
+            System.out.println("Too cold");
+        }
+
     }
 }
