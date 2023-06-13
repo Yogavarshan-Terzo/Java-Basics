e26d7fac9aeb152469a79e34692f2f619d1af734
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 13:32:14 +0530

    Create-Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int [] numbers1 = new int[5];
+        numbers1[0] = 1;
+        numbers1[1] = 2;
+        System.out.println(Arrays.toString(numbers1));
+        int [] numbers2 = {1,2,3,4,5};
+        System.out.println(Arrays.toString(numbers2));
     }
 }
