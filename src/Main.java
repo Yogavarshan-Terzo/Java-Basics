c8e4a8778b536c3421aed26c9be060621963c531
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 17:53:18 +0530

    Complete-Mortgage-Calculator

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,30 @@ public class Main {
         final byte PERCENT = 100;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal :");
+        System.out.print("Principal ($1K to $1000K):");
         int principal = scanner.nextInt();
+        while (!(principal >= 1_000 && principal <= 1_000_000)){
+            System.out.println("Enter amount between 1000 and 1000000");
+            System.out.print("Principal ($1K to $1000K):");
+            principal = scanner.nextInt();
+        }
 
         System.out.print("Annual Interest rate :");
         float annualInterest = scanner.nextFloat();
+        while (!(annualInterest > 0 && annualInterest <= 30)){
+            System.out.println("Enter percentage between 1 and 30");
+            System.out.print("Annual Interest rate :");
+            annualInterest = scanner.nextFloat();
+        }
         float monthlyInterest = annualInterest/PERCENT/MONTH_IN_YEAR;
 
         System.out.print("Period in Years :");
         byte years = scanner.nextByte();
+        while (!(years >= 0 && years <= 30)){
+            System.out.println("Enter years between 1 and 30");
+            System.out.print("Period in Years :");
+            years = scanner.nextByte();
+        }
         int numberOfPayments = years * MONTH_IN_YEAR;
 
         double mortgage = principal * (monthlyInterest * (Math.pow((monthlyInterest + 1),numberOfPayments))/(Math.pow((monthlyInterest + 1),numberOfPayments)-1));
