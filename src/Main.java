8433fdbc3a9a3cd5afe79ba96ca8c1065f23b5fc
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 14:49:08 +0530

    Order-Of-Operators

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10 + 3;
-        x = 10 - 3;
-        x = 10 * 3;
-        x = 10 / 3;//returns rounded off value
-        int y = x++;
-        y = ++x;
-        x += 2;//compound assignment
+        int x = (8 + 2) * 3;
+        System.out.println(x);
     }
 }
