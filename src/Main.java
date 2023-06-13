8211de7a692b5a282b1502e2f5745f8642934bbd
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 14:15:08 +0530

    Create-Multidimensional-Arrays

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         System.out.println(Arrays.toString(numbers1));
         int [] numbers2 = {1,2,3,4,5};
         System.out.println(Arrays.toString(numbers2));
+
+        int [][] array1 = new int [2][3];
+        array1[0][0] = 1;
+        array1[0][1] = 2;
+        System.out.println(Arrays.deepToString(array1));
+        int [][] array2 = {{ 1, 2, 3},{ 4, 5, 6}};
+        System.out.println(Arrays.deepToString(array2));
     }
 }
