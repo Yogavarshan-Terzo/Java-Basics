80b52199e3737bf4ec0a557f5b43f11ab5edbe94
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 15:56:46 +0530

    Number-Formatting

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String price = currency.format(89342.32);
+        System.out.println(price);
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String percentage = percent.format(0.1);
+        System.out.println(percentage);
     }
 }
