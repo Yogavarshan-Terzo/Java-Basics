7acfe2c06bdfea0364cd45cc7cab4892a6c9ebae
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 17:25:10 +0530

    Use-While-Loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")){
+            System.out.println("Input :");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
