673c38ae4b21e0be08bc0f462cf22b0f967b1cde
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 12:54:47 +0530

    Use-Reference-Datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Date todayDate = new Date();
+        System.out.println(todayDate);
     }
 }
