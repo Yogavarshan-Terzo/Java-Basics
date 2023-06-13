c406b624959bc0f99cc8fee6d2a3dbe8bc2d066e
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 15:10:22 +0530

    Type-Casting

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //Implicit casting
+        //byte > short > int > long > float > double
+        short x = 3;
+        int y =  x + 2;
+        //Explicit Casting
+        double a = 1.2;
+        int b = (int)a + 2;
+        //non-compatible casting
+        //Use wrapper class
+        String num1 = "1";
+        Integer num2 = Integer.parseInt(num1);
     }
 }
