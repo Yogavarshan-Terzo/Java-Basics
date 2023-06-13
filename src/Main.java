bc3e1015305b2ff6e191536d8f1d4f53bbc6ed39
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:04:25 +0530

    Use-Scanner-Class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age :");
+        byte x = scanner.nextByte();
+        System.out.println(x);
+        System.out.print("Name :");
+        String name = scanner.nextLine().trim();
+        System.out.println(name);
     }
 }
