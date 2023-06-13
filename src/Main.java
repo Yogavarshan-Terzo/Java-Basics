c1176f7d16ce42218fd8aa5427bdff26b4d662fb
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:27:50 +0530

    Create-Mortgage-Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal :");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest rate :");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest/100/12;
+
+        System.out.print("Period in Years :");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * 12;
+
+        double mortgage = principal * (monthlyInterest * (Math.pow((monthlyInterest + 1),numberOfPayments))/(Math.pow((monthlyInterest + 1),numberOfPayments)-1));
+
+        System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));
     }
 }
