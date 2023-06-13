320f67e320ba0181a06a5b2b104cab5b3bb4de35
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 13:15:15 +0530

    Use-String-Class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String message = "Hello World";
+        System.out.println(message);
+        //concatenation
+        message = message + "!!";
+        System.out.println(message);
+        //length
+        System.out.println(message.length());
+        //endswith,startswith
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith(("H")));
+        //uppercase,lowercase
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        //remove space
+        System.out.println(message.trim());
+        //find index of strings
+        System.out.println(message.indexOf("!!"));
+        //replace
+        System.out.println(message.replace("!","#"));
     }
 }
