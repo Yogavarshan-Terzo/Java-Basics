7f05996ab05bbf6387ae9887dd1259fe6b597ed8
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 12:52:32 +0530

    Use-Primitive-Datatypes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        byte age = 20;
+        short count = 1_000;
+        int viewsCount = 900_456_124;
+        long population = 475_847_388_123L;
+        float price = 10.99F;
+        double totalPrice = 58_453.980;
+        char letter = 'A';
+        boolean isEligible = true;
+
     }
 }
