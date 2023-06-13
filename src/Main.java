ed2f29d11c9ffadb7f59843afd5e23cbe1d09501
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 12:57:25 +0530

    Primitive-Vs-Reference-Datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.awt.*;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int x = 3;
+        int y = x;
+        x = 3;
+        System.out.println(y);
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 3;
+        System.out.println(point2);
     }
 }
