8f958d5fecf0d575fed8f7871cdcbb27273c2f3b
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:42:41 +0530

    Use-Logical-Operators

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int temperature = 25;
+        boolean isWarm = temperature >= 20 && temperature <=35;
+        System.out.println(isWarm);
+
+        boolean hasHighSalary = true;
+        boolean hasGoodCredit = true;
+        boolean isCriminal = false;
+        boolean isEligible = hasGoodCredit || hasHighSalary && !isCriminal;
+        System.out.println(isEligible);
     }
 }
