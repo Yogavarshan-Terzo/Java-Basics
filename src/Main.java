ed3959d3ccc1c3e6c59069aae232a08ac466ecad
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:54:09 +0530

    Use-Ternary-Operator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int income = 100_000;
+        String className = income > 100_000 ? "First" : "Economy";
     }
 }
