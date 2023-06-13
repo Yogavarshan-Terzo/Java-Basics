f2dea4aabecc5766e43795a47b07068b0015dfaf
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 16:37:18 +0530

    Use-Comparision-Operators

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int x = 10;
+        int y = 20;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+
     }
 }
