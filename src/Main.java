3906f8f06ed3df2caf555e51c534249bd0e8aec6
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 13:23:55 +0530

    Use-Escape-Sequences

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //avoid quotation
+        String message = "Hello \"World\"";
+        System.out.println(message);
+        //avoid \
+        String dir = "c:\\Windows\\..";
+        System.out.println(dir);
+        //tab space
+        message = "Hello\tWorld";
+        System.out.println(message);
+        //new line
+        message = "Hello\nWorld";
+        System.out.println(message);
+
     }
 }
