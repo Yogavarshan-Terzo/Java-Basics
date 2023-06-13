80dda01c17ee7504f1b4f1922d574521005a4b05
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-13 14:48:15 +0530

    Arithmetic-Operators

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int x = 10 + 3;
+        x = 10 - 3;
+        x = 10 * 3;
+        x = 10 / 3;//returns rounded off value
+        int y = x++;
+        y = ++x;
+        x += 2;//compound assignment
     }
 }
